20f48ec7ca58f3712b34124428c2015471d0d333
enveal <envealsood@gmail.com>
2020-06-10 07:23:11 +0530

    Project ready to deploy
    Signed-off-by: enveal <envealsood@gmail.com>

--- src/public/app/single-post.js
+++ src/public/app/single-post.js
@@ -2,9 +2,9 @@ let spostId =window.localStorage.spostId;
 function loadSinglePost(){
     $.get(`/api/posts/s/${spostId}`, {}, (posts) => {
         for (let p of posts) { 
-        $('#posts-container').append(
+        $('#leftss').append(
         $(`
-              <div class="col-4">
+              <div class="col-10">
               <div class="card m-2">
               <div class="card-body">
               <h5 class="card-title">${p.title}</h5>
@@ -27,18 +27,16 @@ function loadComments() {
   $.get(`/api/comments/posts/${spostId}`, (comments) => {
     for (let c of comments) {
       console.log('working fine')
-      $('#posts-container').append(
+      $('#rightss').append(
         $(`
-        <div class="col-4">
+        <div class="col-8">
           <div class="card m-2">
             <div class="card-body">
               <h5 class="card-title">${c.title}</h5>
               <h6 class="card-subtitle mb-2 text-muted">${c.user.username}</h6>
               <p class="card-text">
                 ${c.body}
-                <a href="#">...read more</a>
               </p>
-              <a href="#" class="card-link">Comment</a>
               <a href="#" class="card-link">Like</a>
             </div>
           </div>
